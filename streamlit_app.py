7ddfef37257b0e9c7d654bd83c7636b1ac1602ff
agungmagnetcuan-ai <agungmagnetcuan@gmail.com>
2026-05-23 10:14:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -105,3 +105,22 @@ st.success("⚽ Bola berhasil bergerak!")
 import streamlit as st
 
 st.write("Hello, *World!* :sunglasses:")
+
+
+
+
+
+
+import altair as alt
+import pandas as pd
+import streamlit as st
+from numpy.random import default_rng as rng
+
+df = pd.DataFrame(rng(0).standard_normal((200, 3)), columns=["a", "b", "c"])
+chart = (
+    alt.Chart(df)
+    .mark_circle()
+    .encode(x="a", y="b", size="c", color="c", tooltip=["a", "b", "c"])
+)
+
+st.write(chart)
