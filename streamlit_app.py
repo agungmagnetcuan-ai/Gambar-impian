be063b0417e801b60a0cb4f5e82be4a13050df7e
agungmagnetcuan-ai <agungmagnetcuan@gmail.com>
2026-05-23 09:43:07 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
-import streamlit as st
 
-st.title("🎈 impian tuan agung")
+
+
+    import streamlit as st
+
+st.title("🎈 Impian Tuan Agung")
+
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-
+)
